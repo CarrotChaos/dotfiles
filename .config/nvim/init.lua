76f76de350a63f2493f84a373b22f95ad5b86929
CarrotChaos <blackberrycoffeecake@protonmail.com>
2025-11-09 21:40:35 -0700

    add neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,24 @@
+-- init.lua
+
+-- Access vim global object
+local vim = vim
+local Plug = vim.fn['plug#']
+
+-- Plugin section
+vim.call('plug#begin')
+
+Plug('Mofiqul/dracula.nvim')
+
+vim.call('plug#end')
+
+vim.cmd('silent! colorscheme dracula')
+
+-- === Keymaps ===
+-- Press 'jj' in insert mode to escape
+vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
+-- Map Ctrl+c to copy
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
+
+-- Map Ctrl+v to paste
+vim.api.nvim_set_keymap('n', '<C-v>', '"+P', { noremap = true, silent = true })
