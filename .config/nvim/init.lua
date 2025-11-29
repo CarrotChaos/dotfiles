f7709f9f9c9f4c18713d0896391b0b9ab7dc6fef
Your Name <you@example.com>
2025-11-28 21:46:13 -0700

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,3 +22,13 @@ vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
 
 -- Map Ctrl+v to paste
 vim.api.nvim_set_keymap('n', '<C-v>', '"+P', { noremap = true, silent = true })
+
+vim.cmd [[
+augroup RestoreCursorShapeOnExit
+    autocmd!
+    autocmd VimLeave * set guicursor=a:ver25-blinkwait700-blinkoff400-blinkon250
+augroup END
+]]
+
+
+
--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,3 +1,4 @@
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 [ -f ~/.cache/brightness_level ] && brightnessctl set "$(cat ~/.cache/brightness_level)" &
 feh --bg-fill /home/user/Pictures/Wallpapers/vadim-sadovski-n7.jpg &
 picom &
