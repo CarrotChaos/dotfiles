b34f8717aa51d256e5adc7e7a170aac53329e119
Your Name <you@example.com>
2025-12-03 10:37:17 -0700

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
   -- add dracula
-  { "Mofiqul/dracula.nvim" },
+  { "binhtran432k/dracula.nvim" },
 
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
+    lazy = false,
+    priority = 1000,
     opts = {
       colorscheme = "dracula",
     },
