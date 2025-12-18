77bf0dc426bebfd92b911164c12f2e9c36f2c099
Your Name <you@example.com>
2025-12-17 22:18:16 -0700

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,11 @@
--- init.lua
 require('config.options')      -- General options configuration
 require('config.keybinds')     -- Keybindings configuration
-
 
 vim.pack.add({
 	{ src = "https://github.com/binhtddev/dracula.nvim" },
 	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/Saghen/blink.cmp" }, 
+	{ src = "https://github.com/brenoprata10/nvim-highlight-colors" }, 
 })
 
 -- set colorscheme
@@ -17,6 +17,20 @@ require("lualine").setup({
         theme = "dracula"
     }
 })
+
+-- setup blink.cmp
+require("blink.cmp").setup({
+  fuzzy = {
+    implementation = "lua",
+  },
+})
+
+-- highlight colors
+require('nvim-highlight-colors').setup({})
+
+-- language servers
+vim.lsp.enable({"pyright"})
+vim.lsp.enable({"bashls"})
 
 -- set the cursor to be line on exit
 vim.cmd [[
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,4 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
