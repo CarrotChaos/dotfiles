cf70c206e0becfdcbc725f9ad844c6804dd39479
Your Name <you@example.com>
2025-12-17 22:27:40 -0700

    test

--- .config/nvim/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
--- .dwm/autostart.sh
+++
@@ -1,8 +0,0 @@
-/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
-[ -f ~/.cache/brightness_level ] && brightnessctl set "$(cat ~/.cache/brightness_level)" &
-xwallpaper --zoom ~/Pictures/Wallpapers/snow.jpg
-picom &
-dunst &
-dwmblocks &
-~/scripts/statusbar/audio-listen.sh &
-~/scripts/statusbar/minute-listen.sh &
